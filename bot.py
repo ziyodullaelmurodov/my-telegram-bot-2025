87d483184e556312e5994691c1c2da55f73b0f64
MacOS m3 <uzbzarif@gmail.com>
2025-01-16 17:42:50 +0500

    sned methods

--- bot.py
+++ bot.py
@@ -21,3 +21,50 @@ class Bot:
         }
         response = requests.post(self.base_url + "sendMessage", data=data)
         return response.json()
+    
+    def send_photo(self, chat_id, photo):
+        """
+        This method sends a photo to a specific chat 
+
+        Args:
+            chat_id: The id of the chat to send the photo to
+            photo: The path to the photo to send
+        """
+        
+    def send_document(self, chat_id, document):
+        """
+        This method sends a document to a specific chat
+
+        Args:
+            chat_id: The id of the chat to send the document to
+            document: The path to the document to send
+        """
+
+    def send_audio(self, chat_id, audio):
+        """
+        This method sends an audio to a specific chat
+
+        Args:
+            chat_id: The id of the chat to send the audio to
+            audio: The path to the audio to send
+        """
+
+    def send_video(self, chat_id, video):
+        """
+        This method sends a video to a specific chat
+
+        Args:
+            chat_id: The id of the chat to send the video to
+            video: The path to the video to send
+        """
+
+    def send_voice(self, chat_id, voice):
+        """
+        This method sends a voice to a specific chat
+
+        Args:
+            chat_id: The id of the chat to send the voice to
+            voice: The path to the voice to send
+        """
+
+    # Add more methods here
